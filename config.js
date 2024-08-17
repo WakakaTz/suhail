1ca42ad2cba9839aff07eedd718aa3f280870cc1
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2024-08-18 01:13:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
+
 //═══════[Required Variables]════════\\
 global.audio= "" ;  
 global.video= "" ;

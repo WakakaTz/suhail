bc24ed2e0e2c4085ac0d3991cbd4c354475f69b4
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2025-03-11 10:03:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="saimsamsu7@gmail.com"
-global.location="Lahore,Pakistan"
+global.location="Lahore,Pakista"
 
 
 global.mongodb= process.env.MONGODB_URI || ""

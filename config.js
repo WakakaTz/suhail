ab7b835ff4bb29f46af67ab7cace3a7afe0b4cb6
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2025-06-18 08:29:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.allowJids= process.env.ALLOW_JID || "null"
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Tanzania";
+global.timezone= process.env.TZ || process.env.TIME_ZONE || "Tanzania ";
 global.github=process.env.GITHUB|| "";
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
 global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 

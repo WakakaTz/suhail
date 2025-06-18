e0163568cf1b4c642ffed73518be9240f6b9b99c
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2025-06-18 08:19:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.audio= "" ;
 global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
-global.email ="saimsamsu@gmail.com"
+global.email ="saimsams@gmail.com"
 global.location="Lahore,Pakita"
 
 
